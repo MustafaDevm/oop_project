ef38c56164c9f14e1a2d81b8926ba92e0a94a649
mostafa <mustcs23@gmail.com>
2025-02-03 18:38:37 +0200

    correct run.c++

--- run.cpp
+++ run.cpp
@@ -4,8 +4,7 @@
 #include "subject/Subject.h"
 
 void displayMenu()
-{   
-    loop:
+{ 
     std::cout << "School Management System\n";
     std::cout << "1. Add a new Student\n";
     std::cout << "2. Assign a Subject to a Student\n";
@@ -14,31 +13,5 @@ void displayMenu()
     std::cout << "5. View Grades\n";
     std::cout << "6. Exit\n";
     std::cout << "Enter your choice: ";
-    int choice;
-    cin >> choice;
-    Student st;
-    switch (choice)
-    {
-    case 1:
-           
-           int id;cin>>id;
-           st.setID(id);
-        break;
-    case 2:
-        
-        break;
-    case 3:
 
-        break;
-    case 4:
-
-        break;
-    case 5:
-
-        break;
-    case 6:
-
-        break;
-        default: cout<<" chose form given options"; goto loop; break;
-    }
 }
